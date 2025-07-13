26953faf011180ef8949b847c6727132f455eca6
goulashh <x7r85@students.keele.ac.uk>
2025-07-13 17:27:15 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="px-4">
+    <nav class="sticky top-0 z-50 rounded-lg">
+      <router-link active-class="active" to="/"       >Home</router-link> |
+      <router-link active-class="active" to="/about"  >About</router-link> |
+      <router-link active-class="active" to="/games"  >Games</router-link> |
+      <router-link active-class="active" to="/web"    >Web</router-link> |
+      <router-link active-class="active" to="/contact">Contact</router-link>
+    </nav>
+    <router-view/>
+  </div>
+  
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card bg-[var(--transp-b)]">
+    <h1>Jude Radford</h1>
+    <h2>BSc Computer Science (Games)<br/>with Competency in Japanese</h2>
+  </div>
+
+  <div id="about" class="card bg-[var(--transp-a)] p-3">
+    <h2 id="title" class="mx-4">About</h2>
+    <div id="txt">
+      <h3>Jude Radford</h3>
+      <h4>Keele 2025 | 1st in Computer Science (Games) | Competency in Japanese</h4>
+      <p>
+        I'm excited to announce that I've freshly graduated from Keele University after acheiving a first!<br/>
+        My interests include software, games and web development, AI and AI ethics as well as philosophy and cooking.
+
+      </p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/Navbar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
